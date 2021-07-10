767ab97aa0b3c236000321bfbf9f039aabe7bfc0
yifanwu2828 <yiw084@ucsd.edu>
2021-07-10 00:50:23 -0400

    add duration adn get_stats

--- ail/common/utils.py
+++ ail/common/utils.py
@@ -1,4 +1,7 @@
 import random
+from datetime import timedelta
+from time import time
+from typing import Tuple
 
 import numpy as np
 import torch as th
@@ -9,6 +12,16 @@ def set_random_seed(seed: int) -> None:
     np.random.seed(seed)
     th.manual_seed(seed)
     th.cuda.manual_seed(seed)
+
+
+def duration(start_time) -> str:
+    return str(timedelta(seconds=int(time() - start_time)))
+
+
+def get_statistics(x: np.ndarray) -> Tuple[np.ndarray, ...]:
+    if not isinstance(x, np.ndarray):
+        x = np.asarray(x)
+    return x.mean(), x.std(), x.min(), x.max()  # noqa
 
 
 def combined_shape(length: int, shape=None):
