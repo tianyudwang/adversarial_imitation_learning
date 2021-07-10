cf39b1a9515e61f8ce33cec8a6aab4b8028fd98f
yifanwu2828 <yiw084@ucsd.edu>
2021-07-10 19:28:17 -0400

    add test

---
+++ ail/test/math/test_normalize.py
@@ -0,0 +1,20 @@
+import numpy as np
+from common.math import normalize, unnormalize
+
+
+x1 = np.arange(1, 100, 5)
+
+
+def test_normalize():
+    assert abs(np.mean(normalize(x1, x1.mean(), x1.std()))) < 1e-6
+
+
+def test_unnormalize():
+    norm_x = normalize(x1, x1.mean(), x1.std())
+    unnorm_x = unnormalize(norm_x, x1.mean(), x1.std())
+    assert np.allclose(unnorm_x, x1)
+
+
+
+
+
