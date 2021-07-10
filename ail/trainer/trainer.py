2ca4d10c81cfe9fb5d5b0e08a3444e5b189c8cad
yifanwu2828 <yiw084@ucsd.edu>
2021-07-10 00:48:47 -0400

    reformat

--- ail/trainer/trainer.py
+++ ail/trainer/trainer.py
@@ -1,15 +1,10 @@
-
-
-
-class AIL_Trainer():
-
+class AIL_Trainer:
     def __init__(self, params):
         pass
 
         # Initialize hyperparameters
 
         # Initialize environment and agent
-
 
     def run_training_loop(self, n_iter, expert_policy, eval_policy):
 
@@ -22,4 +17,4 @@ class AIL_Trainer():
         # b. Train discriminator to classify expert and agent data with cross-entropy loss
         # c. Update reward with respect to discriminator
         # d. Update policy with respect to updated reward
-        
+        pass
