f0cd1ed115b807837711a3a53e01f3db0a706619
yifanwu2828 <yiw084@ucsd.edu>
2021-07-13 01:11:55 -0400

    rename and add countdown

--- ail/common/utils.py
+++ ail/common/utils.py
@@ -1,6 +1,5 @@
 import random
-from datetime import timedelta
-from time import time
+from time import sleep
 from typing import Tuple
 
 import numpy as np
@@ -14,11 +13,17 @@ def set_random_seed(seed: int) -> None:
     th.cuda.manual_seed(seed)
 
 
-def duration(start_time) -> str:
-    return str(timedelta(seconds=int(time() - start_time)))
+def countdown(t_sec) -> None:
+    while t_sec:
+        mins, secs = divmod(t_sec, 60)
+        time_format = f"{mins: 02d}:{secs: 02d}"
+        print(time_format, end="\r")
+        sleep(1)
+        t_sec -= 1
+    print("Done!!")
 
 
-def get_statistics(x: np.ndarray) -> Tuple[np.ndarray, ...]:
+def get_stats(x: np.ndarray) -> Tuple[np.ndarray, ...]:
     if not isinstance(x, np.ndarray):
         x = np.asarray(x)
     return x.mean(), x.std(), x.min(), x.max()  # noqa
@@ -30,5 +35,5 @@ def combined_shape(length: int, shape=None):
     return (length, shape) if np.isscalar(shape) else (length, *shape)
 
 
-def asarray_shape2(x):
+def asarray_shape2d(x):
     return np.asarray(x, dtype=np.float32).reshape(1, -1)
