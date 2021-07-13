eea66b782c0dd1c2e43a96cd80f2b1a1d81b2054
yifanwu2828 <yiw084@ucsd.edu>
2021-07-13 01:13:05 -0400

    reformat

--- ail/test/math/test_normalize.py
+++ ail/test/math/test_normalize.py
@@ -2,7 +2,7 @@ import numpy as np
 from ail.common.math import normalize, unnormalize
 
 
-x1 = np.arange(1, 100, 5).reshape(-1,1)
+x1 = np.arange(1, 100, 5).reshape(-1, 1)
 
 
 def test_normalize():
@@ -13,8 +13,3 @@ def test_unnormalize():
     norm_x = normalize(x1, x1.mean(), x1.std())
     unnorm_x = unnormalize(norm_x, x1.mean(), x1.std())
     assert np.allclose(unnorm_x, x1)
-
-
-
-
-
